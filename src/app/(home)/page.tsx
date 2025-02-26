02444eec50d62083d497bf61f08d5787ac137e0d
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-02-26 22:11:25 +0500

    server side using trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { trpc } from "@/trpc/client";
 export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "Antonio" });
-  return <div className="">Videos will be displayed here {data?.greeting}</div>;
+  // const { data } = trpc.hello.useQuery({ text: "Antonio" });
+  const { data } = trpc.Sohail.useQuery("Faiz");
+
+  return (
+    <div className="">
+      Videos will be displayed here {data?.greeting} {data?.greeting}
+    </div>
+  );
 }
