b593381173ebc86006889c8d1ddf9ad276690340
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-02-27 21:45:42 +0500

    using best way

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,15 @@
 import { trpc } from "@/trpc/server";
 import { PageClient } from "./client";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
 export default async function Home() {
   void (await trpc.hello.prefetch({ text: "Antonio" }));
 
   return (
-    <div className="">
-      <PageClient />
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PageClient />
+      </Suspense>
+    </HydrateClient>
   );
 }
