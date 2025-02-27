9aadeb69e42cc70e60387252f850e23ec68e2d5c
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-02-27 21:33:17 +0500

    using tRPC with combination of server and client <for best of both word because server give speed and client side give us interactivity>

---
+++ src/app/(home)/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { trpc } from "@/trpc/client";
+
+export const PageClient = () => {
+  const [data] = trpc.hello.useSuspenseQuery({
+    text: "Antonio",
+  });
+  return <div>Page client {data.greeting}</div>;
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,11 @@
 import { trpc } from "@/trpc/server";
+import { PageClient } from "./client";
 export default async function Home() {
-  const data = await trpc.hello({ text: "Antonio" });
-  console.log(data);
+  void (await trpc.hello.prefetch({ text: "Antonio" }));
 
-  return <div className="">Videos will be displayed here {data.greeting}</div>;
+  return (
+    <div className="">
+      <PageClient />
+    </div>
+  );
 }
