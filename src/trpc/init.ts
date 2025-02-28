aaeae95bc3a3a7be48d8eb5932f7ea58d0cacdb1
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-02-28 22:51:36 +0500

    adding protected procedure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,17 +1,19 @@
+import { auth } from '@clerk/nextjs/server';
 import { initTRPC } from '@trpc/server';
 import { cache } from 'react';
 import superjson  from 'superjson';
+import { TRPCError } from '@trpc/server';
+
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: 'user_123' };
+  const {userId} = await auth()
+  return { clerkUserId: userId };
 });
+export type Context = Awaited<ReturnType <typeof createTRPCContext>> 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>(  ).create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
@@ -21,3 +23,14 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts){
+  const {ctx } = opts
+  if(!ctx.clerkUserId){
+    throw new TRPCError({code : "UNAUTHORIZED", message: "You must be logged in to access this resource"})
+  }
+  return opts.next({
+    ctx: {
+      ...ctx,
+    }
+  })
+})
