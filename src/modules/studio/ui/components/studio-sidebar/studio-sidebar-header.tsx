74fc2000986b275d9de36f915f7532f77577e689
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-03-06 04:29:16 +0500

    using user-avatar

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -1,15 +1,38 @@
 import { SidebarHeader } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import UserAvatar from "@/components/user-avatar";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-function SduioSidebarHeader() {
+const SduioSidebarHeader = () => {
   const { user } = useUser();
+  if (!user) {
+    return (
+      <SidebarHeader className="flex items-center justify-center pb-4 gap-y-1">
+        <Skeleton className="size-[112px] rounded-full " />
+        <div className="flex flex-col items-center mt-2">
+          <Skeleton className="h-4 w-[80px] mb-2" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </SidebarHeader>
+    );
+  }
   return (
-    <SidebarHeader>
-      <Link href="/users/current"></Link>
+    <SidebarHeader className="flex items-center justify-center pb-4">
+      <Link href="/users/current">
+        <UserAvatar
+          imgUrl={user.imageUrl}
+          name={user.fullName ?? "User"}
+          className="size-[112px] hover:opacity-80 transition-opacity"
+        />
+      </Link>
+      <div className="flex flex-col items-center mt-2 gap-y-1">
+        <p className="text-sm font-medium">Your Profile</p>
+        <p className="text-xs text-muted-foreground">{user.fullName}</p>
+      </div>
     </SidebarHeader>
   );
-}
+};
 
 export default SduioSidebarHeader;
