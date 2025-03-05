c5e41b09e2ccecdbb061a7b79445c540cdc08978
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-03-05 06:36:11 +0500

    Studio sidebar

--- src/modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,16 +1,46 @@
-import { Sidebar } from "@/components/ui/sidebar";
+"use client";
+import {
+  Sidebar,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import { SidebarContent } from "@/components/ui/sidebar";
-import MainSection from "./main-section";
-import { Separator } from "@/components/ui/separator";
-import PersonalSection from "./personal-section";
+
+import Link from "next/link";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const StudioSidebar = () => {
+  const pathname = usePathname();
   return (
-    <Sidebar collapsible="icon" className="pt-16 z-40 ">
-      <SidebarContent className="bg-background ">
-        <MainSection />
-        <Separator />
-        <PersonalSection />
+    <Sidebar collapsible="icon" className="pt-16 z-40">
+      <SidebarContent className="bg-background">
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === "/studio"}
+                tooltip="Content"
+              >
+                <Link href="/studio" className="w-full">
+                  <VideoIcon className="size-5" />
+                  <span>Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild tooltip="Exit Studio">
+                <Link href="/" className="w-full">
+                  <LogOutIcon className="size-5" />
+                  <span>Exit Studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
